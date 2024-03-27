048185573f4eb496adfd32a5f98289a8b26bbb67
MustafaaMehdi <anfsmus@gmail.com>
2024-03-27 16:22:15 +0100

    add lib modules

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-console.log('stan');
-
 import '../scss/style.scss'
+
+import homeRender from './homeRender.mjs'
+
+let homeBtn = document.getElementById('homeBtn')
+
+homeBtn.addEventListener('click', homeRender)
+homeRender()
