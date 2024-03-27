27e8db24284c775bdb2ebe3a4d74b619bdcbdb86
MustafaaMehdi <anfsmus@gmail.com>
2024-03-27 17:12:50 +0100

    contact info function

--- contactInfo.mjs
+++ contactInfo.mjs
@@ -4,4 +4,7 @@ let mainContainer = document.getElementById('mainContainer')
 
 export default function contactInfo() {
     contactHeader = createElement('h2', 'contactHeader', 'contactHeader', 'Reach out to us!')
+    contactEmailHeader = createElement('h3', 'contactEmailHeader', 'contactEmailHeader', 'Send us an E-mail')
+    contactEmailHeader = createElement('h3', 'contactEmailHeader', 'contactEmailHeader', 'Send us an E-mail')
+
 }
