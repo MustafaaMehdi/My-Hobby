8c8d86cad34cb9bfab38dc23ad5ce92c87b270b8
MustafaaMehdi <anfsmus@gmail.com>
2024-03-27 16:40:42 +0100

    add header

--- contactInfo.mjs
+++ contactInfo.mjs
@@ -2,3 +2,6 @@ let contactBtn = document.getElementById('contactBtn')
 
 let mainContainer = document.getElementById('mainContainer')
 
+export default function contactInfo() {
+    contactHeader = createElement('h2', 'contactHeader', 'contactHeader', 'Reach out to us!')
+}
